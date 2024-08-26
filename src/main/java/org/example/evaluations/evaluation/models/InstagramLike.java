b458d5d2d47ed6911a6080ad6b5cfb59840aeeec
khannaanurag1 <khannaanurag1@gmail.com>
2024-08-26 14:50:37 +0530

    Updated readme and problem

--- src/main/java/org/example/evaluations/evaluation/models/InstagramLike.java
+++ src/main/java/org/example/evaluations/evaluation/models/InstagramLike.java
@@ -1,19 +1,14 @@
 package org.example.evaluations.evaluation.models;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 
 import java.util.UUID;
 
-@Entity
 public class InstagramLike {
     @Id
     private UUID id;
 
-    @ManyToOne
     private InstagramPost post;
 
-    @ManyToOne
     private InstagramUser user;
 }
