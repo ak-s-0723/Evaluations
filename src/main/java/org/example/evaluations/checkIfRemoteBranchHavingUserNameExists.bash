4d087800a7c53ae40fe5ab09f48daf50079efacd
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-17 17:11:57 +0530

    updated script

--- src/main/java/org/example/evaluations/checkIfRemoteBranchHavingUserNameExists.bash
+++ src/main/java/org/example/evaluations/checkIfRemoteBranchHavingUserNameExists.bash
@@ -7,7 +7,7 @@ echo "Fetching latest remote references..."
 git fetch "$REMOTE_NAME"
 
 # Check if the remote branch exists
-if git show-ref --quiet --heads "$BRANCH_NAME"; then
+if git branch -r | grep -q "$REMOTE_NAME/$BRANCH_NAME"; then
     echo "Remote branch '$REMOTE_NAME/$BRANCH_NAME' exists."
 else
     echo "Error : Remote branch '$REMOTE_NAME/$BRANCH_NAME' does not exist."
