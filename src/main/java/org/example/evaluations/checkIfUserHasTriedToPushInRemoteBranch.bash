4bbe845f1141d0ffa5ba4edcff12ab3e62a420ab
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-18 20:45:36 +0530

    updated script

--- src/main/java/org/example/evaluations/checkIfUserHasTriedToPushInRemoteBranch.bash
+++ src/main/java/org/example/evaluations/checkIfUserHasTriedToPushInRemoteBranch.bash
@@ -9,5 +9,5 @@ count=$(history | grep "git push" | wc -l)
 if [ "$count" -gt 0 ]; then
     echo "Found $count 'git push' commands in history."
 else
-    echo "No 'git push' commands found in history."
+    echo "Error : No 'git push' commands found in history."
 fi
