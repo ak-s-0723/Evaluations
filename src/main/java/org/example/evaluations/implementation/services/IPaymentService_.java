56f62e984a0a6fd17e1d07ca9121aac50442e1bb
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-08 00:14:14 +0530

    Readme, Testcases and Problem Statement

--- src/main/java/org/example/evaluations/implementation/services/IPaymentService_.java
+++
@@ -1,5 +0,0 @@
-package org.example.evaluations.implementation.services;
-
-public interface IPaymentService_ {
-    String initiatePayment(String name, String phoneNumber, String email,Double amount,String description);
-}
