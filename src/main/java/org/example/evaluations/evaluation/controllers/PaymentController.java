a45bb68b2777bb1b5030450c22a2696e996d56d8
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-08 00:03:59 +0530

    Only Solution and TestCases

--- src/main/java/org/example/evaluations/evaluation/controllers/PaymentController.java
+++ src/main/java/org/example/evaluations/evaluation/controllers/PaymentController.java
@@ -1,7 +1,10 @@
 package org.example.evaluations.evaluation.controllers;
 
+import org.example.evaluations.evaluation.dtos.InitiatePaymentRequestDto;
 import org.example.evaluations.evaluation.services.IPaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,8 @@ public class PaymentController {
     @Autowired
     private IPaymentService paymentService;
 
-    //Add your API here.
+    @PostMapping("/initiatePayment")
+    public String initiatePayment(@RequestBody InitiatePaymentRequestDto initiatePaymentRequestDto) {
+        return paymentService.initiatePayment(initiatePaymentRequestDto.getName(),initiatePaymentRequestDto.getPhoneNumber(),initiatePaymentRequestDto.getEmail(),initiatePaymentRequestDto.getAmount(),initiatePaymentRequestDto.getDescription());
+    }
 }
