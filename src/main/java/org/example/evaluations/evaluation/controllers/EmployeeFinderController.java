999e69bb86472ff83ebbaf3b11203f1ad97053ee
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-13 13:28:21 +0530

    Only Solution and Unit Tests

--- src/main/java/org/example/evaluations/evaluation/controllers/EmployeeFinderController.java
+++ src/main/java/org/example/evaluations/evaluation/controllers/EmployeeFinderController.java
@@ -1,8 +1,12 @@
 package org.example.evaluations.evaluation.controllers;
 
+import org.example.evaluations.evaluation.dtos.EmployeeFinderRequestDto;
+import org.example.evaluations.evaluation.models.Employee;
 import org.example.evaluations.evaluation.services.EmployeeFinderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class EmployeeFinderController {
@@ -10,5 +14,8 @@ public class EmployeeFinderController {
     @Autowired
     private EmployeeFinderService employeeFinderService;
 
-    //Add your APIs here
+    @PostMapping("/employeeFinder")
+    public Page<Employee> findEmployees(@RequestBody EmployeeFinderRequestDto employeeFinderRequestDto) {
+        return employeeFinderService.findEmployees(employeeFinderRequestDto.getDepartment(), employeeFinderRequestDto.getPageNumber(), employeeFinderRequestDto.getPageSize(), employeeFinderRequestDto.getSortParamList());
+    }
 }
