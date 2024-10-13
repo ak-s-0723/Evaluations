92679d914570b0ea0183c4aab242e0f62d53ae2f
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-13 13:36:20 +0530

    Only Solution and Unit Tests

--- src/main/java/org/example/evaluations/evaluation/controllers/SearchController.java
+++ src/main/java/org/example/evaluations/evaluation/controllers/SearchController.java
@@ -1,8 +1,11 @@
 package org.example.evaluations.evaluation.controllers;
 
+import org.example.evaluations.evaluation.models.Flight;
 import org.example.evaluations.evaluation.services.OpenSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class SearchController {
@@ -10,5 +13,8 @@ public class SearchController {
     @Autowired
     private OpenSearchService openSearchService;
 
-     //Add your API here
+    @GetMapping("/search/{query}")
+    public Page<Flight> getFlightsMatchingSearchQuery(@PathVariable String query, @RequestParam Integer pageSize, @RequestParam Integer pageNumber) {
+        return openSearchService.getFlightsMatchingSearchQuery(query,pageSize,pageNumber);
+    }
 }
