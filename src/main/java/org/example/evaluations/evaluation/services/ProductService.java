4858e5c8d500d623f430ad1d88536d6e4891e676
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-13 14:03:02 +0530

    Solution and unit tests

--- src/main/java/org/example/evaluations/evaluation/services/ProductService.java
+++ src/main/java/org/example/evaluations/evaluation/services/ProductService.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -15,13 +17,33 @@ public class ProductService {
 
     private String PRODUCT_IN_DEMAND = "iphone";
     private final String REDIS_KEY_POPULAR_PRODUCT = "iphone_related";
-    private final String REDIS_KEY_NORMAL_PRODUCT = "products";
 
     public Set<Product> getPopularProducts() {
-        return null;
+        Set<Object> productIdsObject = redisTemplate.opsForSet().members(REDIS_KEY_POPULAR_PRODUCT);
+        Set<Product> products = new HashSet<>();
+
+        Set<String> productIds = productIdsObject.stream()
+                .map(Object::toString)
+                .collect(Collectors.toSet());
+
+        if (productIds != null) {
+            for (String id : productIds) {
+                products.add((Product) redisTemplate.opsForHash().get("products", id));
+            }
+        }
+
+        return products;
     }
 
     public Product addProduct(Product product) {
-        return null;
+        redisTemplate.opsForHash().put("products",product.getId(),product);
+
+        if (product.getTitle().toLowerCase().contains(PRODUCT_IN_DEMAND) ||
+                product.getDescription().toLowerCase().contains(PRODUCT_IN_DEMAND) ||
+                product.getCategory().toLowerCase().contains(PRODUCT_IN_DEMAND)) {
+            redisTemplate.opsForSet().add(REDIS_KEY_POPULAR_PRODUCT, product.getId());
+        }
+
+        return product;
     }
 }
